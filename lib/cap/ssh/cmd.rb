48e915a69f8ff8a23b81439bbf37c88c0eeca276
Adam Luzsi <adamluzsi@gmail.com>
2017-02-08 15:45:40 +0100

    chore: remove auto load try

--- lib/cap/ssh/cmd.rb
+++ lib/cap/ssh/cmd.rb
@@ -1,11 +1,1 @@
-require "cap/ssh/cmd/version"
-if defined?(Capistrano::VERSION)
-  case Capistrano::VERSION
-  when /^2/
-    require(File.join(File.dirname(__FILE__),'cmd','2.rake'))
-  when /^3/
-    require(File.join(File.dirname(__FILE__),'cmd','2.rake'))
-  else
-    raise(NotImplementedError)
-  end
-end
+require 'cap/ssh/cmd/version'
